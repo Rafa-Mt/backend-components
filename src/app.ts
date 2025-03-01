f0de4a7ab4626eded90df0e744a72dbc2e36485f
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-03-01 19:39:08 -0400

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { config as dotenv } from 'dotenv';
+
+dotenv();
+
+const app = express();
+const port = Number(process.env.port);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
