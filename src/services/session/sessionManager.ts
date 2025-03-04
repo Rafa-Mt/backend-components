4f09c44956c2795d8a3d792089cd91b4f812029b
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-03-04 16:08:47 -0400

    feat: implemented session component

---
+++ src/services/session/sessionManager.ts
@@ -0,0 +1,26 @@
+import { sign, verify, SignOptions } from 'jsonwebtoken';
+import { config as dotenv } from 'dotenv';
+dotenv();
+
+interface SessionManagerArgs {
+    secret: string;
+    expiresIn: SignOptions['expiresIn'];
+}
+
+export default class SessionManager<UserType> {
+    private secret: string;
+    private ttl: SignOptions['expiresIn'];    
+
+    constructor({secret, expiresIn}: SessionManagerArgs) {
+        this.secret = secret;
+        this.ttl = expiresIn;
+    }
+
+    public createToken(user: UserType & object): string {
+        return sign(user, this.secret, {expiresIn: this.ttl});
+    }
+
+    public verifyToken(token: string): UserType {
+        return verify(token, this.secret) as UserType;
+    }
+}
